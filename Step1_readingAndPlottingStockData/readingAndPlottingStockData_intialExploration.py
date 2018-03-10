820336b58b77ec62e715b05b310e6e88fdb54246
bnajafi <b.najafi87@gmail.com>
2018-03-10 09:09:13 +0100

    updating the initialExplorationFile

--- Step1_readingAndPlottingStockData/readingAndPlottingStockData_intialExploration.py
+++ Step1_readingAndPlottingStockData/readingAndPlottingStockData_intialExploration.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import pandas as pd 
 import os
+import matplotlib.pyplot as plt
 Repository_folder = "C:/Users/behzad/Dropbox/18 Eduameterial_Python_Finance/0 Udacity_machineLearning4Trading/MachineLearning-for-Trading-PipeLine"
 os.chdir(Repository_folder)
 companySymbol = "AAPL"
@@ -12,3 +13,18 @@ df_AAPL.tail(10)
 df_AAPL["Close"].max()
 
 df_AAPL["Volume"].mean()
+
+plt.figure()
+df_AAPL[["Close","Adj Close"]].plot()
+plt.show()
+plt.xlabel("time")
+plt.legend()
+
+start_date = "2010-01-22"
+end_date = "2010-01-26"
+
+dates = pd.date_range(start_date,end_date)
+df1 = pd.DataFrame(index=dates)
+df1.head()
+companySymbol_2 = "SPY"
+DF_SPY = pd.read_csv("/Data/{}.csv".format(companySymbol_2)
